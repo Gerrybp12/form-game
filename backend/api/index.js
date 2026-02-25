81152d53889dee7d32d9b47484317b3ef804f780
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-02-25 22:19:39 +0700

    try catch error

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,7 @@
-const app = require("../src/app");
-
-module.exports = app;
+try {
+  const app = require("../src/app");
+  module.exports = app;
+} catch (e) {
+  console.error("BOOT_ERROR:", e);
+  throw e;
+}
